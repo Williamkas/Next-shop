b4ddd8d337da26c0756ff2e984c15bb20537807d
William A. Kasbar P <williamkasbar1@gmail.com>
2022-03-29 22:46:55 -0300

    add new src

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
+  env: { //variables de entorno
+    customKey: 'customValue',
+  },
+  basePath: '/dist', //acá se encuentra nuestro proyecto
+  compress: true, //prepara nuestro proyecto
+  async redirects() {
+    return [
+      {
+        source: '/hola',
+        destination: 'https://gndx.dev', //cuando el usuario nos redirecciona a /hola lo enviaremos a https://gndx.dev
+        permanent: true,
+      }
+    ]
+  }
 }
-
-module.exports = nextConfig
